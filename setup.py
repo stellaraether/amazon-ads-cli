df2ae8e92920d8ba879ffec8aec5d69efce6719f
Lunan Li <lunan@stellaraether.com>
2026-04-27 21:06:43 -0700

    chore: bump version to 0.1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="amazon-ads-cli",
-    version="0.1.5",
+    version="0.1.6",
     description="CLI tool for Amazon Advertising API v3",
     author="Lunan Li",
     author_email="lunan@stellaraether.com",
